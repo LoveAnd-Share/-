c645f7197ea38916f54a45b4356970cdf905c49a
LoveAnd-Share <1836167892@qq.com>
2024-12-09 16:12:22 +0800

    使用ant改

---
+++ src/views/user/Tips.vue
@@ -0,0 +1,20 @@
+<template>
+     <a-button @click="info">提示</a-button>
+</template>
+<script>
+import { message } from 'ant-design-vue';
+export default {
+
+    data () {
+        return {
+            tips: '提示'
+        }
+    },
+    methods: {
+        info() {
+            messageApi.info('');
+        }
+        
+    }
+}
+</script>
